f30977cccf4132bc1a9de8ab8def8cf1b25e09d9
tsatcher <ted.satcher@dr-inc.com>
2020-05-11 23:06:33 -0500

    More comments and move some things around.

--- src/DeckOfSet.hs
+++ src/DeckOfSet.hs
@@ -2,6 +2,7 @@ module DeckOfSet where
 
 import qualified Data.List as L
 
+-- Define the features of a Set card
 data Value = Uno
            | Dos
            | Tres
@@ -34,6 +35,7 @@ mateFeature v1 v2 =
           _ -> Nothing
         where vs = (fromEnum v1) + (fromEnum v2)
 
+-- Find the mate of two cards
 mateCard :: Card -> Card -> Maybe Card
 mateCard (v1, c1, d1, p1) (v2, c2, d2, p2) = do
   v3 <- mateFeature v1 v2
@@ -41,12 +43,6 @@ mateCard (v1, c1, d1, p1) (v2, c2, d2, p2) = do
   d3 <- mateFeature d1 d2
   p3 <- mateFeature p1 p2
   return (v3, c3, d3, p3)
-
-hand :: [Card]
-hand = [deck !! 0, deck !! 1, deck !! 2
-       , deck !! 3, deck !! 4, deck !! 5
-       , deck !! 6, deck !! 7, deck !! 8
-       , deck !! 9, deck !! 10, deck !! 11]
 
 -- Get all pairs in a hand.  A hand of 12 cards has 66 pairs.
 allPairs :: [Card] -> [(Card, Card)]
@@ -60,12 +56,14 @@ swapCards (c1, c2)
           | c2 < c1 = (c2, c1)
           | otherwise = (c1, c2)
 
+-- Find a mate of two cards in a hand, if it exists.
 mateInHand' :: [Card] -> (Card, Card) -> Maybe (Card, Card, Card)
 mateInHand' h (c1, c2) =
     case (mateCard c1 c2) of
       Nothing -> Nothing
       Just c3 -> if c3 `elem` h then (Just (c1, c2, c3)) else Nothing
 
+-- If a hand has a Set, return the first one.
 handHasASet :: [Card] -> Maybe (Card, Card, Card)
 handHasASet h =
     let pairs = allPairs h
@@ -76,7 +74,7 @@ handHasASet h =
          (c:_) -> c
 
 
--- Lists of cards
+-- A few lists of cards
 troublesomeHand :: [Card]
 troublesomeHand = [(Dos, Red, Blank, Pill), (Tres, Purple, Blank, Pill)
                   , (Tres, Purple, Hash, Pill), (Dos, Green, Blank, Pill)
@@ -91,3 +89,9 @@ deck = [ (v, c, d, p) |
          c <- [Green, Purple, Red],
          d <- [Blank, Hash, Solid],
          p <- [Angles, Pill, Worm]]
+
+hand :: [Card]
+hand = [deck !! 0, deck !! 1, deck !! 2
+       , deck !! 3, deck !! 4, deck !! 5
+       , deck !! 6, deck !! 7, deck !! 8
+       , deck !! 9, deck !! 10, deck !! 11]
