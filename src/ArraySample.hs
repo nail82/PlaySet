751ca52689eb8732823badf7257a715cd1a1d6d4
tsatcher <ted.satcher@dr-inc.com>
2020-05-17 14:41:14 -0500

    Initial import of array sampler.

---
+++ src/ArraySample.hs
@@ -0,0 +1,11 @@
+module ArraySample where
+
+import System.Random
+import Data.List as L
+
+sampleList :: [a] -> a
+sampleList xs = undefined
+
+
+sampleNList :: Int -> [a] -> [a]
+sampleNList = undefined
